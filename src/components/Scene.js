49a79cdd7b81158568e2e8259c45e3d61acd42f9
Maksim Brazhnik <m.brazhnik@tekrum.pr>
2019-10-09 11:11:47 +0300

    add this component

---
+++ src/components/Scene.js
@@ -0,0 +1,11 @@
+import * as THREE from "three";
+
+const Scene = (function() {
+  return {
+    init: function(container) {
+      container.innerHTML = "";
+    }
+  };
+})();
+
+export default Scene;
